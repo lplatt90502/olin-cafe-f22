caa10b6ef6722f1f2f6fb29a52bac9a9d76d057d
lplatt90502 <98347449+lplatt90502@users.noreply.github.com>
2022-10-03 19:19:37 -0400

    Added values

--- labs/01_game_of_life/hdl/decoder_3_to_8.sv
+++ labs/01_game_of_life/hdl/decoder_3_to_8.sv
@@ -4,6 +4,14 @@ module decoder_3_to_8(ena, in, out);
   input wire ena;
   input wire [2:0] in;
   output logic [7:0] out;
-
-
+logic in_bar
+always_comb begin
+    out[7] = in[0] & in[1] & in[2] & ena
+    out[6] = in[0]  &in[1] & ~in[2] & ena
+    out[5] = in[0]& ~in[1] & in[2] & ena
+    out[4] = in[0] & ~in[1] & ~in[2] & ena
+    out[3] = ~in[0] & in[1] & in[2] & ena
+    out[2] = ~ in[0] & in[1] & ~ in[2] & ena
+    out[1] = ~in[0] & ~in[1] & in[2] & ena
+    out[0] = ~in[0] & ~in[1] & ~in[2] & ena
 endmodule
